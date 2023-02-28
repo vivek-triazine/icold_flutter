a8f65aa56fa21533a7de67a9860b7c353e423612
Tspl <abhishekg@triazinesoft.com>
2023-02-28 15:47:48 +0530

    code

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -13,9 +13,9 @@ class HomeView extends GetView<HomeController> {
         title: const Text('HomeView'),
         centerTitle: true,
       ),
-      body: Center(
+      body: const Center(
         child: Text(
-          'HomeView is working',
+          'New  Task',
           style: TextStyle(fontSize: 20),
         ),
       ),
