cc314c7f61a282f2ad60900d068241688cc563bb
Vivek <viveky@triazinesoft.com>
2023-03-06 10:42:37 +0530

    code update

--- lib/app/modules/home/views/home_view.dart
+++ lib/app/modules/home/views/home_view.dart
@@ -10,7 +10,7 @@ class HomeView extends GetView<HomeController> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Voow '),
+        title: const Text('VVVVVV'),
         centerTitle: true,
       ),
       body: const Center(
